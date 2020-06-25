6b46e248b5979ddf36ab8031b80a6a2967a6e102
ugwu collins <collinsuchinaka@gmail.com>
2020-06-25 08:55:58 +0100

    index setup for redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import combinedReducers from './reducers/index';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from  'redux-thunk';
 
+
+const store =  createStore(combinedReducers, applyMiddleware(thunk));
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
